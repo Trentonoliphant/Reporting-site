206c0e93c2dd7561c8190a4d866919e333e7061c
Trent Oliphant <trent@067-trentoliphant.local>
2014-10-17 15:09:10 -0500

    Simple layout of weekly hours

--- ReportingSite/Reports/views.py
+++ ReportingSite/Reports/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.utils import timezone
 
-from Reports.models import Weekly_report
+from Reports.models import Weekly_report, Hours_report
 
 class ReportView(generic.ListView):
 	template_name = 'Reports/reports.html'
@@ -11,8 +11,24 @@ class ReportView(generic.ListView):
 	def get_queryset(self):
 		return Weekly_report.objects.all()
 
-class DetailView(generic.DetailView):
-	model = Weekly_report
+class DetailView(generic.ListView):
+	model = Hours_report
 	template_name = 'Reports/detail.html'
 
 	context_object_name = 'report'
+
+	def get_queryset(self):
+		weeks = {'4':'2014-10-01','3':'2014-10-14'}
+
+		week = weeks[self.kwargs['pk']]
+		return Hours_report.objects.filter(Week_of=week).order_by('Employee')
+
+
+def get_week(request, year, month, day):
+	date = "{}-{}-{}".format(year,month,day)
+
+	hours = Hours_report.objects.filter(Week_of=date)
+
+	output={'hours':hours}
+
+	return render()
